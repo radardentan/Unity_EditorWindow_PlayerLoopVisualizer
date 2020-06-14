df717ffaf32eb901c5475f9cd34767d0adba3660
radar_dentan <42970783+radardentan@users.noreply.github.com>
2020-06-14 18:23:09 +0900

    スクリプト本体をうp

---
+++ PlayerLoopVisualizer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.LowLevel;
+
+public class PlayerLoopVisualizer : EditorWindow
+{
+    static PlayerLoopSystem playerLoopSystem;
+    static Vector2 scrollPos;
+    bool firstCall = true;
+    GUIStyle leftBold;
+    GUIStyle normalCenter;
+
+
+    [MenuItem("Window/PlayerLoopVisualizer")]
+    public static void Open() 
+    {
+        GetWindow<PlayerLoopVisualizer>();
+        Init();
+    }
+
+    protected void OnGUI()
+    {
+        playerLoopSystem = PlayerLoop.GetCurrentPlayerLoop();
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+        
+        if (firstCall)
+        {
+            leftBold = new GUIStyle()
+            {
+                alignment = TextAnchor.MiddleLeft,
+                fontStyle = FontStyle.Bold
+            };
+            normalCenter = new GUIStyle()
+            {
+                alignment = TextAnchor.MiddleCenter
+            };
+            firstCall = false;
+        }        
+
+        foreach (var header in playerLoopSystem.subSystemList) 
+        {
+            EditorGUILayout.BeginVertical(GUI.skin.box);
+            GUILayout.Label(header.type.Name, leftBold);
+
+            foreach (var subSystem in header.subSystemList) 
+            {
+                GUILayout.Label(subSystem.type.Name, normalCenter);
+            }
+
+            EditorGUILayout.EndVertical();
+        }
+        EditorGUILayout.EndScrollView();
+        
+    }
+
+    static void Init()
+    {
+        playerLoopSystem = PlayerLoop.GetDefaultPlayerLoop();
+    }
+
+}
